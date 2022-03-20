86a91be7f2cc7357af88b27fcc51875f77294c3f
francomattt <franco.pirondo@gmail.com>
2022-03-20 09:50:13 -0300

    commit rama2 1

--- src/Hola_mundo.java
+++ src/Hola_mundo.java
@@ -4,6 +4,7 @@ public class Hola_mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("holaMundo");
+		System.out.println("segunda rama");
 	}
 
 }
